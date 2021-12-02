f0479915b066266c1f4c26fc5afbc3a4e92ec67f
Rafael Anholeto <rafaanholeto3@gmail.com>
2021-12-02 12:44:12 -0300

    Lição de casa - MÓDULO 6

---
+++ Lição - Módulo 6/Script.sql
@@ -0,0 +1,73 @@
+insert into patients 
+(id, nome, telefone, birthdate)
+values
+(1, 'João Gomes', '94455-9875', '10/05/1970'),
+(2, 'Giva Jones', '91575-7715', '28/03/1965'),
+(3, 'Joel Silva', '94585-7855', '15/08/1983')
+
+
+alter table patients 
+add column birthdate varchar(12)
+
+select * from patients p 
+
+insert into hospitalization_history 
+(date_and_entry_patient_hour, date_and_exit_patient_hour, description, response_doctor, id_patient)
+values
+('10/11/2016 - 18:40', '10/11/2016 - 20:35', 'Flu problem', 5555, 2);
+
+
+alter table hospitalization_history 
+add constraint fk_responseDoctor
+foreign key (response_doctor)
+references doctors(matricula)
+
+alter table hospitalization_history 
+add column response_doctor varchar(50)
+
+insert into doctors 
+(nome, department, cargo, matricula)
+values
+('José de Carvalho', 1, 'Ortopedista', 1111),
+('Renan Santos', 2, 'Cardiologista', 2222),
+('Marcelo Dias', 2, 'Oftalmologista', 3333),
+('Francisco Souza', 3, 'Psiquiatra', 4444),
+('Davi Campos', 3, 'Pneumologista', 5555);
+
+select * from doctors d 
+
+select * from hospitalization_history hh 
+
+create table doctors(
+matricula varchar(50) primary key,
+nome varchar(50),
+department int,
+cargo varchar(50),
+telefone varchar(12)
+);
+
+
+
+select d."nomeMedico" , department from doctors d 
+
+select * from doctors d 
+
+select * from doctors d 
+inner join hospitalization_history hh 
+on d.matricula = hh.response_doctor
+inner join patients p 
+on p.id = hh.id_patient
+order by p.id asc
+
+select * from patients p 
+inner join hospitalization_history hh 
+on p.id = hh.id_patient
+order by p.id asc
+
+
+
+
+ 
+
+ 
+
